52219ab77ca6e672a9865b283e32ae47efe26797
Q___ <dillon.jones125@gmail.com>
2017-05-22 08:05:09 -0500

    First Master Commit
    This is the first commit with the game now rendering level-01 and the GUI.

---
+++ desktop/src/com/dijdesign/libgdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.dijdesign.libgdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dijdesign.libgdx.game.CanyonBunnyMain;
+
+public class DesktopLauncher {
+	
+	public static void main (String[] arg) {
+	LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+	config.height = 480;
+	config.width = 800;
+	
+	new LwjglApplication(new CanyonBunnyMain(), config);
+	}
+}
