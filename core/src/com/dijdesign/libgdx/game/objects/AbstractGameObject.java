b4e6240ed199096ed424d2be37a26a602483bb0f
Q___ <dillon.jones125@gmail.com>
2017-05-24 08:36:27 -0500

    Created goldCoin
    Set up AbstractGameObject class to take in some physics such as velocity, friction, and acceleration. Also began set-up of the collision bounds. After I created the goldCoin's class so that it can be instantiated into the level.

--- core/src/com/dijdesign/libgdx/game/objects/AbstractGameObject.java
+++ core/src/com/dijdesign/libgdx/game/objects/AbstractGameObject.java
@@ -2,6 +2,8 @@ package com.dijdesign.libgdx.game.objects;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.MathUtils;
 
 public abstract class AbstractGameObject {
 
@@ -10,6 +12,13 @@ public abstract class AbstractGameObject {
 	public Vector2 origin;
 	public Vector2 scale;
 	public float rotation;
+	
+	public Vector2 velocity;
+	public Vector2 terminalVelocity;
+	public Vector2 friction;
+
+	public Vector2 acceleration;
+	public Rectangle bounds;
 
 	public AbstractGameObject () {
 		position = new Vector2();
@@ -17,9 +26,50 @@ public abstract class AbstractGameObject {
 		origin = new Vector2();
 		scale = new Vector2(1, 1);
 		rotation = 0;
+		
+		velocity = new Vector2();
+		terminalVelocity = new Vector2(1,1);
+		friction = new Vector2();
+		acceleration = new Vector2();
+		bounds = new Rectangle();
+	}
+	
+	protected void updateMotionX(float deltaTime){
+		if (velocity.x != 0){
+			//Apply Friction
+			if (velocity.x > 0){
+				velocity.x = Math.max(velocity.x - friction.x * deltaTime, 0);
+			} else {
+				velocity.x = Math.min(velocity.x + friction.x * deltaTime, 0);
+			}
+		}
+		//Apply Acceleration
+		velocity.x += acceleration.x * deltaTime;
+		//Make sure the object's velocity does not exceed max or min terminal velocity
+		velocity.x = MathUtils.clamp(velocity.x, -terminalVelocity.x, terminalVelocity.x);
+	}
+	
+	protected void updateMotionY(float deltaTime){
+		if (velocity.y != 0){
+			//Apply Friction
+			if (velocity.y > 0){
+				velocity.y = Math.max(velocity.y - friction.y * deltaTime, 0);
+			} else {
+				velocity.y = Math.min(velocity.y + friction.y * deltaTime, 0);
+			}
+		}
+		//Apply Acceleration
+		velocity.y += acceleration.x * deltaTime;
+		//Make sure the object's velocity does not exceed max or min terminal velocity
+		velocity.y = MathUtils.clamp(velocity.x, -terminalVelocity.x, terminalVelocity.x);
 	}
 
 	public void update (float deltaTime) {
+		updateMotionX(deltaTime);
+		updateMotionY(deltaTime);
+		//Move to new position
+		position.x += velocity.x * deltaTime;
+		position.y += velocity.y * deltaTime;
 	}
 
 	public abstract void render (SpriteBatch batch);
